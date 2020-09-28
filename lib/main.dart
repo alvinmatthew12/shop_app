bbac76ea66867a61b2c6ec34fcff89c1ee6c262f
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-09-29 00:56:40 +0700

    Add style and theme in the app

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
+        accentColor: Colors.red,
+        fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a shop!'),
-      ),
     );
   }
 }
