1895e97f7d10a365e2f1d3aa33455bc360d44f80
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-09-30 00:58:33 +0700

    Transform ChangeNotifierProvider builder to ChangeNotifierProvider.value on ProductsGrid

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -12,8 +12,8 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
-      itemBuilder: (context, index) => ChangeNotifierProvider(
-        create: (c) => products[index],
+      itemBuilder: (context, index) => ChangeNotifierProvider.value(
+        value: products[index],
         child: ProductItem(),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
