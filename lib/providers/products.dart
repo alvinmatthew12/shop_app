5980b8f496e6596754b08e8cb4431f7797cca163
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-10-04 23:58:48 +0700

    Saving new product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -45,4 +45,17 @@ class Products with ChangeNotifier {
   Product findById(String id) {
     return _items.firstWhere((product) => product.id == id);
   }
+
+  void addProduct(Product product) {
+    final newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
+    // _items.insert(0, newProduct); // at the start of the list
+    notifyListeners();
+  }
 }
