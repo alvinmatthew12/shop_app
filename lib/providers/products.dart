9579a677a874b68ba5a0b4654cf07a380ef70284
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-10-06 00:12:33 +0700

    Add http and send http request

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -47,6 +50,18 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    final url = 'https://flutter-shop-app-54163.firebaseio.com/products.json';
+    http.post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'price': product.price,
+        'imageUrl': product.imageUrl,
+        'isFavorite': product.isFavorite,
+      }),
+    );
+
     final newProduct = Product(
       id: DateTime.now().toString(),
       title: product.title,
