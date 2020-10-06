ccc11181fcb70b7be10c2baecbdcd0ca3749808e
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-10-06 21:07:07 +0700

    Update the favorite status

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
-import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -18,8 +20,30 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void togglerFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> togglerFavoriteStatus() async {
+    final oldStatus = isFavorite;
+
     isFavorite = !isFavorite;
     notifyListeners();
+
+    final url = 'https://flutter-shop-app-54163.firebaseio.com/products/$id.json';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
