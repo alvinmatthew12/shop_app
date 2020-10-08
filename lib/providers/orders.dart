afae1b8e7b021c1e4674f1b9270d4656d4ad7f82
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-10-09 01:07:35 +0700

    Fetching order from API

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -26,6 +26,33 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchOrders() async {
+    final url = 'https://flutter-shop-app-54163.firebaseio.com/orders.json';
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+        id: orderId,
+        amount: orderData['amount'],
+        dateTime: DateTime.parse(orderData['dateTime']),
+        products: (orderData['products'] as List<dynamic>)
+            .map((item) => CartItem(
+                  id: item['id'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                  title: item['title'],
+                ))
+            .toList(),
+      ));
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrders(List<CartItem> cartProducts, double total) async {
     final url = 'https://flutter-shop-app-54163.firebaseio.com/orders.json';
     final timestamp = DateTime.now();
